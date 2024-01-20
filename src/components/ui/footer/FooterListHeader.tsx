40ea8598f8b07ae1f9d6fda31a01105d52cdcfc4
osigie <kenosagie88@gmail.com>
2024-01-20 01:37:33 +0100

    remove hardcoded fontsize and weight

--- src/components/ui/footer/FooterListHeader.tsx
+++ src/components/ui/footer/FooterListHeader.tsx
@@ -3,11 +3,9 @@ import Typography from "@mui/material/Typography";
 export const FooterListHeader = ({ title }: { title: string }) => {
   return (
     <Typography
-      variant="subtitle2"
-      fontWeight={700}
+      variant="h5"
       textAlign="left"
       color="custom.main"
-      fontSize="16px"
     >
       {title}
     </Typography>
