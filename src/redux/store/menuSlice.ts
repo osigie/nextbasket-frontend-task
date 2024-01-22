7744f6e12a7e8ab71acced267f0a2557fc015d56
osigie <kenosagie88@gmail.com>
2024-01-22 12:52:49 +0100

    create menu slice for app state

---
+++ src/redux/store/menuSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type initialStateType = {
+  isMenuActive: boolean;
+  drawer: boolean;
+  currentDrawerState: "cart" | "wishlist";
+};
+
+const initialState: initialStateType = {
+  isMenuActive: false,
+  drawer: false,
+  currentDrawerState: "cart",
+};
+
+const menuSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    toggleMenuActive: (state) => {
+      state.isMenuActive = !state.isMenuActive;
+    },
+
+    toggleDrawerOpen: (state, action) => {
+      state.drawer = true;
+      state.currentDrawerState = action.payload;
+      state.isMenuActive = false;
+    },
+
+    toggleDrawerClose: (state) => {
+      state.drawer = false;
+    },
+  },
+});
+
+export const menuReducer = menuSlice.reducer;
+export const { toggleMenuActive, toggleDrawerClose, toggleDrawerOpen } =
+  menuSlice.actions;
