f72db0dc9a8a50ea813d3cb7ea0ecfe41ac1737a
osigie <kenosagie88@gmail.com>
2024-01-22 12:52:10 +0100

    create redux store

---
+++ src/redux/app/store.ts
@@ -0,0 +1,60 @@
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import {
+  Action,
+  combineReducers,
+  configureStore,
+  ThunkAction,
+} from "@reduxjs/toolkit";
+import { productApi } from "../services/product.service";
+import { cartReducer } from "../store/cartSlice";
+import { wishlistReducer } from "../store/wishlistSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { menuReducer } from "../store/menuSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  wishlist: wishlistReducer,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: {
+    app: persistedReducer,
+    menu: menuReducer,
+    [productApi.reducerPath]: productApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat([productApi.middleware]),
+});
+
+setupListeners(store.dispatch);
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+
+export const persistor = persistStore(store);
