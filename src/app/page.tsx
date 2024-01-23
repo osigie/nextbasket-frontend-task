df4730ecb6145138d414ad83e1209947554d48fa
osigie <kenosagie88@gmail.com>
2024-01-23 03:52:28 +0100

    remove unused dispatch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,6 @@ import Link from "next/link";
 export default function HomePage() {
   const { isMenuActive } = useAppSelector((state) => state.menu);
 
-  const dispatch = useAppDispatch();
 
   const [productList, setProductList] = React.useState<
     ProductsResponseT | undefined
